a5ceb1ccc55974c913ffe78acdc075ba3e31e148
mugi789 <mugifadilah01@gmail.com>
2022-10-01 23:19:40 +0700

    little change

--- Python/last-ip-number-checker.py
+++ Python/last-ip-number-checker.py
@@ -7,7 +7,10 @@ while hitung < 256:
     try:
         ip = '.'.join(map(str, host.split(".")[:-1]+[hitung]))
         get = requests.get('http://'+ip, timeout=0.25)
-        print('http://'+ip+"/ "+str(get.status_code)+"\033[32m OK    \033[39m")
+        if get.status_code == 200:
+            print('http://'+ip+"/ "+str(get.status_code)+"\033[32m OK    \033[39m")
+        else:
+            print('http://'+ip+"/ "+str(get.status_code)+"\033[31m Error     \033[39m", end='\r')
         hitung += 1
     except requests.exceptions.ConnectionError:
         print('http://'+ip+"/ 404\033[31m Error     \033[39m", end='\r')
